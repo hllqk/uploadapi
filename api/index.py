989ac385eeab0309afca82ac733f33651f24e662
hllqk <shuia@shuia.tk>
2022-08-22 03:56:55 +0000

    zz

--- api/index.py
+++ api/index.py
@@ -1,12 +1,9 @@
 from flask import Flask
-import sys
-application = Flask(__name__)
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+   return 'Hello World'
 
-@application.route("/")
-def hello():
-    return "Hello hllqk!"
-
-
-if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=int(sys.argv[1]))
+if __name__ == '__main__':
+   app.run()
